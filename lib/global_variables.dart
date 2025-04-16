a4b51c37a7f4a8ddab9306028841149ebc3b7453
M Talha Naeem <talhanaeem4u@gmail.com>
2025-04-16 11:15:36 +0400

    Second Commit

---
+++ lib/global_variables.dart
@@ -0,0 +1,45 @@
+final products = [
+  {
+    'id': '0',
+    'title': 'Jordan Shoes',
+    'price': 120.69,
+    'imageUrl': 'assets/images/shoes_4.png',
+    'company': 'Nike',
+    'sizes': [8, 9, 10],
+  },
+  {
+    'id': '1',
+    'title': 'Bata Women\'s Shoes',
+    'price': 128.95,
+    'imageUrl': 'assets/images/shoes_3.png',
+    'company': 'Bata',
+    'sizes': [8, 9, 10],
+  },
+  {
+    'id': '2',
+    'title': 'Men\'s Nike Shoes',
+    'price': 144.52,
+    'imageUrl': 'assets/images/shoes_1.png',
+    'company': 'Nike',
+    'sizes': [9, 10, 11, 12],
+  },
+  {
+    'id': '3',
+    'title': 'Adidas Shoes',
+    'price': 120.12,
+    'imageUrl': 'assets/images/shoes_2.png',
+    'company': 'Adidas',
+    'sizes': [9, 10, 12],
+  },
+];
+
+final cart = [
+  {
+    'id': '3',
+    'title': 'Adidas Shoes',
+    'price': 120.12,
+    'imageUrl': 'assets/images/shoes_2.png',
+    'company': 'Adidas',
+    'size': 9,
+  },
+];
